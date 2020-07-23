f22fdc249b77a82ee6aacc9cf3116c3de13c0c7b
kick1995 <68591670+kick1995@users.noreply.github.com>
2020-07-23 13:22:51 +0530

    Add files via upload

---
+++ trail.py
@@ -0,0 +1,28 @@
+from flask import Flask, request
+from twilio.twiml.messaging_response import MessagingResponse
+import coviddata as cv
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello, World!"
+
+@app.route("/sms", methods=['POST'])
+def sms_reply():
+    """Respond to incoming calls with a simple text message."""
+    # Fetch the message
+    msg = request.form.get('Body')
+    a,c,d,r,dp,rp=cv.state(msg)
+    # Create reply
+    resp = MessagingResponse()
+    resp.message("acha: {}".format(msg))
+    resp.message(a)
+    resp.message(c)
+    resp.message(d)
+    resp.message(r)
+    resp.message(dp)
+    resp.message(rp)
+    return str(resp)
+
+if __name__ == "__main__":
+    app.run(debug=True)
